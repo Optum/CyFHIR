313956d0e4ef09ffe9d673968a9338d8d4680373
mfrisby3 <matthew.frisby@optum.com>
2020-05-07 14:44:17 -0400

    add starter gatsby with seo metadata

---
+++ docs/gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: `CyFHIR`,
+    description: `Homepage of CyFHIR: a Neo4j plugin for FHIR.`,
+    siteUrl: `https://optum.github.io/CyFHIR/`
+  },
+  pathPrefix: "/CyFHIR",
+  plugins: [
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
