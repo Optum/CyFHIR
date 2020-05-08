540d1b692a888835312f76f8c9fa0bdecac8dbd4
mfrisby3 <matthew.frisby@optum.com>
2020-05-08 12:57:23 -0400

    APM Server missing message broke, this fixes it

--- express/src/config.ts
+++ express/src/config.ts
@@ -2,33 +2,32 @@ import appRoot from 'app-root-path';
 import dotenv from 'dotenv'
 import apm from 'elastic-apm-node'
 
-function loggerError(log) {
-    // If APM Server is missing, only display error once
-    const logs = new Set();
-    if (log.match(/APM Server transport error/g) && !logs.has(log)) {
-        logs.add(log);
-        process.stdout.write('APM Server Missing\n');
-    }
+function loggerError(error, log) {
+  // If APM Server is missing, only display error once
+  if (error.match(/APM Server transport error/g) && !log.has(error)) {
+    log.add(error);
+    process.stdout.write('APM Server Missing\n');
+  }
 }
 
-
 function config() {
-    try {
-        apm.start({
-            serviceName: 'cyfhir_plugin',
-            serverUrl: 'http://apm-server:8200'
-        });
-        apm.logger.error = (log) => loggerError(log)
-    } catch (error) {
-        console.log(error);
-    }
-    
-    const filename = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
-    const path = `${appRoot}/${filename}`;
-    
-    console.log(`Loading configuration from ${path}`);
-    dotenv.config({
-        path
+  const logs = new Set();
+  try {
+    apm.start({
+      serviceName: 'cyfhir_plugin',
+      serverUrl: 'http://apm-server:8200'
     });
+    apm.logger.error = (error) => loggerError(error, logs)
+  } catch (error) {
+    console.log(error);
+  }
+
+  const filename = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+  const path = `${appRoot}/${filename}`;
+
+  console.log(`Loading configuration from ${path}`);
+  dotenv.config({
+    path
+  });
 }
 export default config;
