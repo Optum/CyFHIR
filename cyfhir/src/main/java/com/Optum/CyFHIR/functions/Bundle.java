d0c210f023a70dbd6fa9ecd6fafe9e1854028666
Matthew Frisby <matthew.frisby@optum.com>
2020-06-12 21:00:23 -0400

    Property bug (#16)
    * Neo4j driver had unexpected data resolution format for integers because Neo4j uses 64-bit signed Integers and Node.js does not. Resolved with appropriate flag in instantiation of Neo4j driver in the express server.
    * Swagger Doc example bundle contained resources that were more than one degree away from example patient which made testing for accuracy of bundles before and after entering the Database difficult. Resolved by removing those resources.
    * Bug in cyfhir.bundle.build() where Arrays of Numerics were being left as Arrays of strings and was not up to FHIR standard

--- cyfhir/src/main/java/com/Optum/CyFHIR/functions/Bundle.java
+++ cyfhir/src/main/java/com/Optum/CyFHIR/functions/Bundle.java
@@ -1,19 +1,22 @@
 package com.Optum.CyFHIR.functions;
 
 import com.Optum.CyFHIR.procedures.Convert;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.neo4j.graphdb.Path;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
-import java.io.IOException;
+
+import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("ALL")
 public class Bundle {
 
     @UserFunction("cyfhir.bundle.build")
     @Description("cyfhir.bundle.build(List<Path>)) parses nodes into FHIR bundle given a collected Tree of connected entries")
-    public Map build(@Name("mapResponse") List<Path> paths) throws IOException {
+    public Map build(@Name("mapResponse") List<Path> paths) {
 
         Convert tree = new Convert();
         List<Map<String, Object>> mapResponse = tree.toTree(paths, true, new HashMap<String, Object>())
@@ -22,13 +25,10 @@ public class Bundle {
 
         //build empty bundle
         Map bundle = new HashMap();
+        bundle.put("resourceType", "Bundle");
+        bundle.put("type", "transaction");
+        List<Map> entriesList = new ArrayList<Map>();
         if (!mapResponse.get(0).isEmpty()) {
-            bundle.put("resourceType", "Bundle");
-            bundle.put("type", "transaction");
-
-            //create empty list of entries
-            List<Map> entriesList = new ArrayList<Map>();
-
             //traverse each entry in the mapResponse and map with parseResource
             for (Map entry : mapResponse) {
                 Map entryMap = new HashMap();
@@ -41,39 +41,33 @@ public class Bundle {
                 //add parsed entry to list of entries
                 entriesList.add(entryMap);
             }
-            bundle.put("entry", entriesList);
         }
+        bundle.put("entry", entriesList);
         return bundle;
     }
 
+    //Convert tree response to FHIR via this algorithm using the previously injected metadata
     public Map parseResources(Map resource) {
-        //remove custom fields/data cleanup
-        resource.remove("_resourceId");
-        resource.remove("_id");
-        resource.remove("_type");
-        resource.remove("_isArray");
+        resource = removeMetadata(resource);
         Map resourceMap = new HashMap();
         //traverse resource keys and map values accordingly
         for (Object key : resource.keySet()) {
-            //map each nested resources if within an array - although
-            if (resource.get(key) instanceof ArrayList) {
-                ArrayList<Map> mapList = new ArrayList<Map>();
-                //extract type field to set as the key
-                String type = ((ArrayList<Map>) resource.get(key)).get(0).get("_type").toString();
-                //find if array is true array
-                Boolean isArray = (Boolean) ((ArrayList<Map>) resource.get(key)).get(0).get("_isArray");
+            Object keyValue = resource.get(key);
+            if (keyValue instanceof ArrayList) {
+                ArrayList<Map> mapList = new ArrayList<>();
+                Map nestedData = ((ArrayList<Map>) keyValue).get(0);
+                String type = nestedData.get("_type").toString();
+
+                Boolean isArray = (Boolean) nestedData.get("_isArray");
                 //map flat keys if resource is not a true array
                 if (!isArray) {
-                    resourceMap.put(type, parseResources(((ArrayList<Map>) resource.get(key)).get(0)));
+                    resourceMap.put(type, parseResources(nestedData));
                 } else {
                     //if true array map each sub resource
-                    for (Map subResource : (ArrayList<Map>) resource.get(key)) {
-                        //remove custom fields/data cleanup
-                        subResource.remove("_type");
-                        subResource.remove("_id");
-                        subResource.remove("_isArray");
-                        subResource.remove("_resourceId");
-                        mapList.add(parseResources(subResource));
+                    for (Map subResource : (ArrayList<Map>) keyValue) {
+                        subResource = removeMetadata(subResource);
+                        Map subResourceParsed = parseResources(subResource);
+                        mapList.add(subResourceParsed);
                     }
                     resourceMap.put(type, mapList);
                 }
@@ -81,17 +75,33 @@ public class Bundle {
             //if a flat key, then map value
             else {
                 //to convert arrays stored as Strings in neo4j properties
-                if (resource.get(key).toString().matches("\\[.+(, .+)*]")) {
-                    resourceMap.put(key, Arrays.asList(resource.get(key).toString().substring(1, resource.get(key).toString().length() - 1).split(",")).stream().map(item -> {
-                        return item.trim();
-                    }));
-                }
-                //map string, long, double, and boolean values
-                else {
-                    resourceMap.put(key, resource.get(key));
+                if (keyValue.toString().matches("\\[.+(, .+)*]")) {
+                    List<String> strings = Arrays.asList(keyValue.toString().substring(1, keyValue.toString().length() - 1).split(","));
+                    List<Serializable> serializedArray = strings.stream().map(item -> parseArrayString(item)).collect(Collectors.toList());
+                    resourceMap.put(key, serializedArray);
+                } else {
+                    resourceMap.put(key, keyValue);
                 }
             }
         }
         return resourceMap;
+    }
+
+    //remove metadata fields/data cleanup
+    public Map removeMetadata(Map resource) {
+        resource.remove("_resourceId");
+        resource.remove("_id");
+        resource.remove("_type");
+        resource.remove("_isArray");
+        return resource;
+    }
+
+    //handle arrays of numerical and textual types
+    public Serializable parseArrayString(String item) {
+        String trimmed = item.trim();
+        if (NumberUtils.isCreatable(trimmed)) {
+            return NumberUtils.createNumber(trimmed);
+        }
+        return trimmed;
     }
 }
