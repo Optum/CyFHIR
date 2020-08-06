b8f0205c0a7e759ef31a158d1d164700c8317af8
MatthewFrisby <matthew.frisby@optum.com>
2020-08-05 21:15:39 -0500

    fix script

--- bin/build-cyfhir.sh
+++ bin/build-cyfhir.sh
@@ -4,6 +4,7 @@ set -e
 
 (
 cd cyfhir
+[ ! -d "./plugins" ] && mkdir ./plugins
 mvn clean
 mvn install -U -DskipTests
 mvn -o dependency:tree | grep ":.*:.*:.*" | cut -d] -f2- | sed 's/:[a-z]*$//g' | sed -n '1!p' | sed '$d' > dependencies.txt
